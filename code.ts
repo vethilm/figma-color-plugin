897f0dc6e250b7135f3e35fae1a2ec363050b446
vethilm <burnsc376@gmail.com>
2024-10-25 14:39:12 -0400

    made form

--- code.ts
+++ code.ts
@@ -1,7 +1,12 @@
-figma.showUI(__html__);
 
+figma.showUI(__html__, {width:320,height:300, title: "color pallete generator"});
 
-
-
-
-figma.closePlugin();
+//listen for message
+figma.ui.onmessage = msg => {
+    if(msg.type === 'actionStart'){
+        figma.closePlugin('Colors generated Succesfully');
+    }
+    else if(msg.type==='exit'){
+        figma.closePlugin();
+    }
+}
